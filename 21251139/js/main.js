44eb543c3d52eb691ef378f90bd29e7a896f3ddc
busandcardriver <ayseb8963@gmail.com>
2022-05-23 19:28:57 +0300

    MySpicesDairy

---
+++ 21251139/js/main.js
@@ -0,0 +1,16 @@
+document.getElementById("test").innerHTML=""
+
+function myFunction() {
+    document.getElementById("frm1").submit();}
+
+    function myFunction() {
+        var x = document.getElementById("site").href;
+        document.getElementById("demo").innerHTML = x;
+      }
+
+    
+
+function getOption() {
+    var obj = document.getElementById("benimSecimim");
+    document.getElementById("demo").innerHTML = 
+    obj.options[obj.selectedIndex].text;}
